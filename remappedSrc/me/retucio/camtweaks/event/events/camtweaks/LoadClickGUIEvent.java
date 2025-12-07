4f816381c302c4f0cb4f2eb34398bf07dd4e9e21
retuci0 <retucio@gmail.com>
2025-12-07 09:01:21 +0100

    upsi

--- remappedSrc/me/retucio/camtweaks/event/events/camtweaks/LoadClickGUIEvent.java
+++
@@ -1,5 +0,0 @@
-package me.retucio.camtweaks.event.events.camtweaks;
-
-import me.retucio.camtweaks.event.Event;
-
-public class LoadClickGUIEvent extends Event {}
--- remappedSrc/me/retucio/camtweaks/event/events/camtweaks/LoadCommandManagerEvent.java
+++
@@ -1,5 +0,0 @@
-package me.retucio.camtweaks.event.events.camtweaks;
-
-import me.retucio.camtweaks.event.Event;
-
-public class LoadCommandManagerEvent extends Event {}
